fdfd1869efbc6925af3cede4619c488ca675e7a6
Passive <20432486+PassiveModding@users.noreply.github.com>
2025-06-22 14:54:15 +1000

    Support new light exports

--- MeddleTools/shader_fix.py
+++ MeddleTools/shader_fix.py
@@ -130,11 +130,24 @@ def handleLightFix(light: bpy.types.Object):
     if lightData is None:
         return {'CANCELLED'}
     
-    if "LightType" in light:                        
+    if "LightType" in light:
+        if light["LightType"] in ["SunLight", "MoonLight", "AmbientLight"]:
+            newLight = bpy.data.lights.new(name=light.name, type='SUN')
+            newLight.energy = light["HDRIntensity"]
+            rgbCol = light["ColorRGB"]
+            newLight.color = [rgbCol["X"], rgbCol["Y"], rgbCol["Z"]]
+            
+            light.data = newLight  # replace the light data with the new one
+            # set rotation to quaternion identity
+            light.rotation_mode = 'QUATERNION'
+            light.rotation_quaternion = (1.0, 0.0, 0.0, 0.0)  # identity quaternion
+            
+            bpy.data.lights.remove(lightData)  # remove old light data       
+            return 
         if light["LightType"] == "AreaLight":                            
             newLight = bpy.data.lights.new(name=light.name, type='AREA')                           
             newLight.size = light["ShadowNear"]
-            newLight.energy = light["HDRIntensity"]
+            newLight.energy = light["HDRIntensity"] * 10
             rgbCol = light["ColorRGB"]
             newLight.color = [rgbCol["X"], rgbCol["Y"], rgbCol["Z"]]                   
             newLight.use_custom_distance = True
@@ -145,20 +158,23 @@ def handleLightFix(light: bpy.types.Object):
             light.data = newLight                            
             # remove old light
             bpy.data.lights.remove(lightData)
+            return 
         if light["LightType"] == "PointLight":                            
             lightData.use_custom_distance = True
             lightData.cutoff_distance = light["Range"]
             lightData.shadow_soft_size = light["ShadowNear"]
             lightData.use_soft_falloff = False
             lightData.use_shadow = False
-            lightData.energy = light["HDRIntensity"]
+            lightData.energy = light["HDRIntensity"] * 10
+            return 
         if light["LightType"] == "SpotLight":                            
             lightData.use_custom_distance = True
             lightData.cutoff_distance = light["Range"]
             lightData.shadow_soft_size = light["ShadowNear"]
             lightData.use_soft_falloff = False
             lightData.use_shadow = False
-            lightData.energy = light["HDRIntensity"]
+            lightData.energy = light["HDRIntensity"] * 10
+            return
         if light["LightType"] == "CapsuleLight":
             newLight = bpy.data.lights.new(name=light.name, type='AREA')      
             newLight.shape = 'RECTANGLE'         
@@ -175,3 +191,4 @@ def handleLightFix(light: bpy.types.Object):
             light.data = newLight                            
             # remove old light
             bpy.data.lights.remove(lightData)
+            return
