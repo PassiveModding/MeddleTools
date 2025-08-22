9d6fccfe89d317bfa27aa4cadcdc7e3737fc3c92
Passive <20432486+PassiveModding@users.noreply.github.com>
2025-08-22 19:52:00 +1000

    Set collection of objects

--- MeddleTools/gltf_import.py
+++ MeddleTools/gltf_import.py
@@ -4,7 +4,83 @@ from os import path
 from . import node_configs
 from . import blend_import
 from . import lighting
+
+
+def getRootObject(obj: bpy.types.Object) -> bpy.types.Object:
+    if obj is None:
+        return None
     
+    # find the root object, which is the first parent that has no parent
+    while obj.parent is not None:
+        obj = obj.parent
+    
+    return obj
+
+def unlinkFromSceneCollection(obj: bpy.types.Object, context: bpy.types.Context):
+    if obj is None or context is None:
+        return {'CANCELLED'}
+    
+    # if object is not in the scene collection, do nothing
+    if obj.name not in context.scene.collection.objects:
+        return {'CANCELLED'}
+    
+    # unlink the object from the scene collection
+    context.scene.collection.objects.unlink(obj)
+    
+    return {'FINISHED'}
+
+   
+def addToGroup(obj: bpy.types.Object, group_name: str, context: bpy.types.Context):
+    if obj is None or group_name is None or context is None:
+        return {'CANCELLED'}
+    
+    # check if the group exists
+    if group_name not in bpy.data.collections:
+        # create the group
+        new_collection = bpy.data.collections.new(group_name)
+        context.scene.collection.children.link(new_collection)
+        
+    # check if the object is already in the group
+    if obj.name in bpy.data.collections[group_name].objects:
+        print(f"Object {obj.name} is already in the group {group_name}.")
+        return {'CANCELLED'}
+    
+    # link the object to the group
+    collection = bpy.data.collections[group_name]
+    collection.objects.link(obj)
+    
+    # unlink from scene collection
+    unlinkFromSceneCollection(obj, context)
+    
+    return {'FINISHED'}
+      
+def setCollection(obj: bpy.types.Object, context: bpy.types.Context):
+    if obj is None or context is None:
+        return {'CANCELLED'}
+    
+    # get the root object
+    checkObj = getRootObject(obj)
+    
+    # based on name of the object, set the collection
+    if checkObj.name.startswith("Decal_"):
+        addToGroup(obj, "Decals", context)
+            
+    if checkObj.name.startswith("Light_") or checkObj.name.startswith("EnvLighting_"):
+        addToGroup(obj, "Lights", context)
+        
+    if checkObj.name.startswith("SharedGroup_"):
+        addToGroup(obj, "SharedGroups", context)        
+        
+    if checkObj.name.startswith("Housing_"):
+        addToGroup(obj, "Housing", context)   
+        
+    if checkObj.name.startswith("BgPart_"):
+        addToGroup(obj, "BgParts", context)
+        
+    if checkObj.name.startswith("Terrain_"):
+        addToGroup(obj, "Terrain", context)
+        
+    return {'FINISHED'} 
 
 class ModelImport(bpy.types.Operator):
     bl_idname = "meddle.import_gltf"
@@ -34,7 +110,11 @@ class ModelImport(bpy.types.Operator):
                 print(f"GLTF Path: {filepath}")
                 
                 cache_dir = path.join(path.dirname(filepath), "cache")
-                bpy.ops.import_scene.gltf(filepath=filepath, bone_heuristic='TEMPERANCE')                
+                bpy.ops.import_scene.gltf(filepath=filepath, bone_heuristic='TEMPERANCE')
+                
+                for obj in context.selected_objects:                    
+                    setCollection(obj, context)
+                
                 imported_meshes = [obp for obp in context.selected_objects if obp.type == 'MESH']
 
                 for mesh in imported_meshes:
